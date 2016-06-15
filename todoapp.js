cc0a3a63261a55e01696c4a6b8355c8b75aa4151
baurine <baurine@ekohe.com>
2016-06-15 21:39:33 +0800

    feat: step 3, componentify

--- todoapp.js
+++ todoapp.js
@@ -3,25 +3,53 @@
 
 const { Component } = React
 
+class AddTodo extends Component {
+  render() {
+    return (
+      <div>
+        <input type='input'/>
+        <button>Add Todo</button>
+      </div>
+    )
+  }
+}
+
+class TodoList extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Learn Redux</li>
+          <li>Go Shopping</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+class TodoFilter extends Component {
+  render() {
+    return (
+      <p>
+        Show:{' '}
+        <span>All</span>{' '}
+        <a href='#'>Active</a>{' '}
+        <a href='#'>Completed</a>
+      </p>
+    )
+  }
+}
+
 class TodoApp extends Component {
   render() {
     return (
       <div>
 
-        <input type='input'/>
-        <button>Add Todo</button>
+        <AddTodo/>
 
-        <ul>
-          <li>Learn Redux</li>
-          <li>Go Shopping</li>
-        </ul>
+        <TodoList/>
 
-        <p>
-          Show:
-          <span>All</span>{' '}
-          <a href='#'>Active</a>{' '}
-          <a href='#'>Completed</a>
-        </p>
+        <TodoFilter/>
 
       </div>
     )
