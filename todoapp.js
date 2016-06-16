4da3739699a9d2c4411bbd42db6779b8c2995bc9
baurine <baurine@ekohe.com>
2016-06-16 16:37:45 +0800

    feat: create filter Link component

--- todoapp.js
+++ todoapp.js
@@ -125,15 +125,59 @@ class TodoList extends Component {
 
 class TodoFilter extends Component {
   render() {
+    const { currentFilter } = this.props
+    
     return (
       <p>
-        Show:{' '}
-        <span>All</span>{' '}
-        <a href='#'>Active</a>{' '}
-        <a href='#'>Completed</a>
+        Show:
+        {' '}
+        <Link 
+          filter='SHOW_ALL' 
+          currentFilter={currentFilter}>
+          All
+        </Link>
+        {' '}
+        <Link 
+          filter='SHOW_ACTIVE' 
+          currentFilter={currentFilter}>
+          Active
+        </Link>
+        {' '}
+        <Link 
+          filter='SHOW_COMPLETED' 
+          currentFilter={currentFilter}>
+          Completed
+        </Link>
       </p>
     )
   }
+}
+
+class Link extends Component {
+  
+  render() {
+    const { filter, currentFilter, children } = this.props
+    
+    if (filter === currentFilter) {
+      return (
+        <span>{children}</span>
+      )
+    }
+    
+    return (
+      <a href='#'
+         onClick={(e)=>{
+           e.preventDefault()
+           store.dispatch(filterTodo(filter))
+         }}>
+        {children}
+      </a>
+    )
+  }
+}
+Link.propTypes = {
+  filter: React.PropTypes.string,
+  currentFilter: React.PropTypes.string
 }
 
 /////////////////////////////////////////////////
